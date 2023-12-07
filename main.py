5b079f44eefbfbd3a2d6ad23cd0d1f9a43f7fb09
Austen LeBeau <austenlebeau@gmail.com>
2023-12-07 22:51:55 +0000

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+
+import string
+import sys
+
+sum = 0
+
+with open(sys.argv[1], "r") as file:
+
+    for line in file:
+    
+        digits = list(filter(lambda ch: ch in string.digits, line))
+        
+        if len(digits) == 0:
+            continue
+
+        number = int(digits[0] + digits[-1])
+        sum += number
+
+print(sum)
